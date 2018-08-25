77c9f723e9b0ff8ab13847ecd34661d4ffba6491
xwp <xwp_fullstack@163.com>
2018-08-25 21:49:42 +0800

    把app包里的__init__.py中的app设置成工厂模式，用于生产不同的app.对生产模式进行了优化，使得只有数据的时候才会连接数据库，减少性能消耗。

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 # Author:Dreamer
 # Time:2018.6.1
-import redis
+from redis import StrictRedis
 from flask import Flask
 
 from flask_session import Session
@@ -16,6 +16,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import config_dict
 
+# 当app没有值的时候，我们创建一个空的数据库db对象
+db = SQLAlchemy()
+# 将redis数据库对象暴露给外界调用
+# # type: StrictRedis作用： 事先声明redis_store以后要保存什么类型的数据
+redis_store = None # type: StrictRedis
+
 #  外界使用方式:  create_app("development") --> 开发模式的app
 #              create_app("production")  --> 线上模式的app
 def create_app(config_name):
@@ -25,12 +31,19 @@ def create_app(config_name):
     # 2.把配置类注册到app里
     ConfigClass = config_dict[config_name]
     app.config.from_object(ConfigClass)
-    # 3.创建mysql数据库对象
-    db = SQLAlchemy(app)
-    # 4.创建redis对象
-    redis_store = redis.StrictRedis(host=ConfigClass.REDIS_HOST, port=ConfigClass.REDIS_PORT,db=ConfigClass.REDIS_NUM)
+    # 3. 创建mysql数据库对象
+    # 懒加载，延迟加载
+    db.init_app(app)
+
+    # 4. 创建redis数据库对象 延迟加载
+    global redis_store
+    # decode_responses=True 从redis中获取的值是str类
+    redis_store = StrictRedis(host=ConfigClass.REDIS_HOST, port=ConfigClass.REDIS_PORT,
+                              db=ConfigClass.REDIS_NUM, decode_responses=True)
     # 5. 开启csrf后端保护验证机制
     # 提取cookie中的csrf_token和ajax请求头里面csrf_token进行比较验证操作
     csrf = CSRFProtect(app)   # session不由表单携带而是放在redis数据库里，这里为什么要这样是个大知识点
     # 6.创建session拓展类的对象(将session的存储调整到redis中)
     Session(app)
+
+    return app
--- manage.py
+++ manage.py
@@ -4,7 +4,12 @@
 # Time:2018.6.1
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app,db
+from app import create_app,db
+
+
+# 单一职责的原则：manage.py 仅仅作为项目启动文件即可
+# 工厂方法的调用（ofo公司）
+app = create_app("development")
 
 # 7. 创建manager管理类
 manager = Manager(app)
