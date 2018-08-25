13cbfe124bf0c271c5bb7a26205d4deee2a3bfb5
xwp <xwp_fullstack@163.com>
2018-08-25 22:05:30 +0800

    在 app包下的 __init__.py 文件中添加日志配置的相关方法.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@
 # -*- coding:utf-8 -*-
 # Author:Dreamer
 # Time:2018.6.1
+import logging
+from logging.handlers import RotatingFileHandler
+
 from redis import StrictRedis
 from flask import Flask
 
@@ -47,3 +50,23 @@ def create_app(config_name):
     Session(app)
 
     return app
+
+
+def create_log(config_name):
+    """记录日志的配置函数"""
+    # 设置日志的记录等级
+    # config_dict[config_name].LOG_LEVEL 获取开发环境日志级别
+    logging.basicConfig(level=config_dict[config_name].LOG_LEVEL)  # 调试debug级
+
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 *100, backupCount=10)
+
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    # INFO manage.py 18 错误信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
