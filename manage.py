23f158cfefa50b97baad06280dc8f0f6d95f9ecf
xwp <xwp_fullstack@163.com>
2018-08-25 20:22:18 +0800

    在Congiig类村配置了mysql数据库的相关信息，并把配置类注册到app里

--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@
 # Time:2018.6.1
 from flask import Flask
 
-
+import pymysql
+pymysql.install_as_MySQLdb()
+from flask_sqlalchemy import SQLAlchemy
 
 
 # 定义配置类
@@ -13,10 +15,19 @@ class Config(object):
     # 开启debug模式
     DEBUG = True
 
+    # 设置mysql数据库
+    # 设置mysql数据库的连接
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    # 接连之前必须创建一个information数据库，否则会报错
+
+    # 关闭数据库的跟踪模式
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 
 @app.route('/')
