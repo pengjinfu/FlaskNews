055f3e9b71b72847e155a50bab7f44f5a31ad571
xwp <xwp_fullstack@163.com>
2018-08-25 21:40:57 +0800

    把app包里的__init__.py中的app设置成工厂模式，用于生产不同的app

--- app/__init__.py
+++ app/__init__.py
@@ -14,17 +14,23 @@ pymysql.install_as_MySQLdb()
 
 from flask_sqlalchemy import SQLAlchemy
 
-from config import Config
-# 1.创建flask对错
-app = Flask(__name__)
-# 2.把配置类注册到app里
-app.config.from_object(Config)
-# 3.创建mysql数据库对象
-db = SQLAlchemy(app)
-# 4.创建redis对象
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT,db=Config.REDIS_NUM)
-# 5. 开启csrf后端保护验证机制
-# 提取cookie中的csrf_token和ajax请求头里面csrf_token进行比较验证操作
-csrf = CSRFProtect(app)   # session不由表单携带而是放在redis数据库里，这里为什么要这样是个大知识点
-# 6.创建session拓展类的对象(将session的存储调整到redis中)
-Session(app)
+from config import config_dict
+
+#  外界使用方式:  create_app("development") --> 开发模式的app
+#              create_app("production")  --> 线上模式的app
+def create_app(config_name):
+    """生产app的工厂方法"""
+    # 1.创建flask对错
+    app = Flask(__name__)
+    # 2.把配置类注册到app里
+    ConfigClass = config_dict[config_name]
+    app.config.from_object(ConfigClass)
+    # 3.创建mysql数据库对象
+    db = SQLAlchemy(app)
+    # 4.创建redis对象
+    redis_store = redis.StrictRedis(host=ConfigClass.REDIS_HOST, port=ConfigClass.REDIS_PORT,db=ConfigClass.REDIS_NUM)
+    # 5. 开启csrf后端保护验证机制
+    # 提取cookie中的csrf_token和ajax请求头里面csrf_token进行比较验证操作
+    csrf = CSRFProtect(app)   # session不由表单携带而是放在redis数据库里，这里为什么要这样是个大知识点
+    # 6.创建session拓展类的对象(将session的存储调整到redis中)
+    Session(app)
