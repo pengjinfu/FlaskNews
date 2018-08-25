af030dc8b70f81a0d5593d4c48c7602ab564f442
xwp <xwp_fullstack@163.com>
2018-08-25 21:06:46 +0800

    对mangage.py的配置类进行抽取,把它放在了config.py文件里了。

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@
 # Author:Dreamer
 # Time:2018.6.1
 import redis
-from redis import StrictRedis
 from flask import Flask
 
 from flask_session import Session
@@ -18,43 +17,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-# 定义配置类
-class Config(object):
-    """工程配置类"""
-    # 开启debug模式
-    DEBUG = True
+from config import Config
 
-    # 设置mysql数据库
-    # 设置mysql数据库的连接
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
-    # 接连之前必须创建一个information数据库，否则会报错
-
-    # 关闭数据库的跟踪模式
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # 设置redis数据库
-    # 设置redis的主机地址
-    REDIS_HOST = "127.0.0.1"
-    # 设置redis的端口
-    REDIS_PORT= 6379
-    # 设置redis使用哪个空间
-    REDIS_NUM  = 1
-
-    # 设置加密字符串
-    SECRET_KEY ="/W73UULUS4UFO5omviuVZz6+Bcjs5+2nRdvmyYNq1wEryZsMeluALSDGxGnuYoKX"
-    # 调整session存储的位置（存储到redis里）
-    # 以下的配置均可在Session的源码看到，或者可以找到官网查询
-
-    # 指明session存储到哪种数据库
-    SESSION_TYPE = "redis"
-    # 上面的指明的数据库的实例对象
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_NUM)
-    # session数据需要加密
-    SESSION_USE_SIGNER = True
-    # 不设置永久存储
-    SESSION_PERMANENT = False
-    # 默认存储的有效时长 （没有调整之前默认值是timedelta(days=31)）
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 
