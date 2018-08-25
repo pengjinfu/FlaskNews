525b5ee006e85639cf521bb41c7f8b85697df4e9
xwp <xwp_fullstack@163.com>
2018-08-25 22:26:22 +0800

    在manage.py里使用日志，并创建同级的logs文件夹，用于存储的log日志。并运行了程序进行了检查。

--- app/__init__.py
+++ app/__init__.py
@@ -25,10 +25,34 @@ db = SQLAlchemy()
 # # type: StrictRedis作用： 事先声明redis_store以后要保存什么类型的数据
 redis_store = None # type: StrictRedis
 
+
+def create_log(config_name):
+    """记录日志的配置函数"""
+    # 设置日志的记录等级
+    # config_dict[config_name].LOG_LEVEL 获取开发环境日志级别
+    logging.basicConfig(level=config_dict[config_name].LOG_LEVEL)  # 调试debug级
+
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 *100, backupCount=10)
+
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    # INFO manage.py 18 错误信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
+
+
 #  外界使用方式:  create_app("development") --> 开发模式的app
 #              create_app("production")  --> 线上模式的app
 def create_app(config_name):
     """生产app的工厂方法"""
+
+    # 0.记录日志
+    create_log(config_name)
     # 1.创建flask对错
     app = Flask(__name__)
     # 2.把配置类注册到app里
@@ -52,21 +76,3 @@ def create_app(config_name):
     return app
 
 
-def create_log(config_name):
-    """记录日志的配置函数"""
-    # 设置日志的记录等级
-    # config_dict[config_name].LOG_LEVEL 获取开发环境日志级别
-    logging.basicConfig(level=config_dict[config_name].LOG_LEVEL)  # 调试debug级
-
-    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
-    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 *100, backupCount=10)
-
-    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
-    # INFO manage.py 18 错误信息
-    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
-
-    # 为刚创建的日志记录器设置日志记录格式
-    file_log_handler.setFormatter(formatter)
-
-    # 为全局的日志工具对象（flask app使用的）添加日志记录器
-    logging.getLogger().addHandler(file_log_handler)
--- manage.py
+++ manage.py
@@ -2,10 +2,11 @@
 # -*- coding:utf-8 -*-
 # Author:Dreamer
 # Time:2018.6.1
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from app import create_app,db
 
+from flask_script import Manager
+from app import create_app, db
+from flask_migrate import Migrate, MigrateCommand
+import logging
 
 # 单一职责的原则：manage.py 仅仅作为项目启动文件即可
 # 工厂方法的调用（ofo公司）
@@ -18,12 +19,21 @@ Migrate(app, db)
 # 将迁移命令添加到管理对象中
 manager.add_command("db", MigrateCommand)
 
+
 @app.route('/')
 def index():
     return "index"
 
 
 if __name__ == '__main__':
+
+    logging.debug("debug的日志信息")
+    logging.info("info的日志信息")
+    logging.warning("warning的日志信息")
+    logging.error("error的日志信息")
+    logging.critical("critical的日志信息")
+
+    # # 在 Flask框架 中，其自己对 Python 的 logging 进行了封装，在 Flask 应用程序中，可以以如下方式进行输出 log:
+    # current_app.logger.info("使用flask封住好的方法 info的日志信息")
     # python manage.py runserver -p -h
     manager.run()
-
