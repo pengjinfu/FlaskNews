27f582806539b9ec0a06782874764e6ef86ee0bf
xwp <xwp_fullstack@163.com>
2018-08-25 20:56:41 +0800

    session数据不由表单提交，而是保存在redis里，在Config里进行了Session保存设置

--- manage.py
+++ manage.py
@@ -6,6 +6,8 @@ import redis
 from redis import StrictRedis
 from flask import Flask
 
+from flask_session import Session
+
 from flask_wtf.csrf import CSRFProtect
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -34,6 +36,22 @@ class Config(object):
     # 设置redis使用哪个空间
     REDIS_NUM  = 1
 
+    # 设置加密字符串
+    SECRET_KEY ="/W73UULUS4UFO5omviuVZz6+Bcjs5+2nRdvmyYNq1wEryZsMeluALSDGxGnuYoKX""
+    # 调整session存储的位置（存储到redis里）
+    # 以下的配置均可在Session的源码看到，或者可以找到官网查询
+
+    # 指明session存储到哪种数据库
+    SESSION_TYPE = "redis"
+    # 上面的指明的数据库的实例对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_NUM)
+    # session数据需要加密
+    SESSION_USE_SIGNER = True
+    # 不设置永久存储
+    SESSION_PERMANENT = False
+    # 默认存储的有效时长 （没有调整之前默认值是timedelta(days=31)）
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
 
 
 
@@ -48,8 +66,9 @@ db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT,db=Config.REDIS_NUM)
 # 5. 开启csrf后端保护验证机制
 # 提取cookie中的csrf_token和ajax请求头里面csrf_token进行比较验证操作
-csrf = CSRFProtect(app)
-
+csrf = CSRFProtect(app)   # session不由表单携带而是放在redis数据库里，这里为什么要这样是个大知识点
+# 6.创建session拓展类的对象(将session的存储调整到redis中)
+Session(app)
 
 
 @app.route('/')
